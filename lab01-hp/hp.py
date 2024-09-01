2dd3e9188eee4dbde1c92e98baee0bd23a6c68c1
michael-cox <2.cox.michael@gmail.com>
2024-08-31 22:37:27 -0500

    format hp.py

--- lab01-hp/hp.py
+++ lab01-hp/hp.py
@@ -1,27 +1,31 @@
 import random
 
+
 # can't apply if you're full hp or don't have any
 def can_apply_potion(hp, num_potions):
     # write here...
 
     return True
-    
+
+
 # apply healing to hp and return hp
 def apply_potion(hp, healing_amount):
     # write here...
 
     return hp
-    
+
+
 # apply dmg to hp and return hp
 def apply_attack(hp, dmg):
     # write here...
 
     return hp
 
+
 # the last one standing wins
 def print_winner(character_hp, enemy_hp):
     # write here...
-    
+
     return
 
 
@@ -29,21 +33,22 @@ def prompt_for_action():
     while True:
 
         # enumerate potential actions
-        print('[1] attack')
-        print('[2] take a potion')
-        print('[3] inspect inventory')
-        print('[4] inspect enemy health')
+        print("[1] attack")
+        print("[2] take a potion")
+        print("[3] inspect inventory")
+        print("[4] inspect enemy health")
 
         # get choice
         try:
-            choice = int(input('what action do you take? '))
+            choice = int(input("what action do you take? "))
             print()
             if choice > 0 and choice < 5:
                 return choice
         except:
             pass
-    
+
     return choice
+
 
 def main():
 
@@ -53,23 +58,25 @@ def main():
 
     # character potions
     num_character_potions = 2
-    
+
     # intro
-    print('welcome to the dimmsdale dimmadimmadimmadome, where you will fight to your death')
-    print('an enemy approaches...')
+    print(
+        "welcome to the dimmsdale dimmadimmadimmadome, where you will fight to your death"
+    )
+    print("an enemy approaches...")
 
     # main loop
     while character_hp > 0 and enemy_hp > 0:
 
         # character choice
         choice = prompt_for_action()
-        
+
         # prompt for choices until player takes a "real" action
         while choice != 1 and choice != 2:
             if choice == 3:
-                print(f'potions: {num_character_potions}')
+                print(f"potions: {num_character_potions}")
             if choice == 4:
-                print(f'enemy health: {enemy_hp}')
+                print(f"enemy health: {enemy_hp}")
             choice = prompt_for_action()
 
         # apply action effects
@@ -80,13 +87,14 @@ def main():
                 character_hp = apply_potion(character_hp, random.randint(3, 8))
                 num_character_potons -= 1
             else:
-                print('you spent a turn rummaging around for a non-existent potion...')
+                print("you spent a turn rummaging around for a non-existent potion...")
 
         # character receives an attack
-        print(f'enemy attacks player... character health: {character_hp}')
+        print(f"enemy attacks player... character health: {character_hp}")
         character_hp = apply_attack(character_hp, random.randint(7, 19))
 
     print_winner(character_hp, enemy_hp)
+
 
 if __name__ == "__main__":
     main()
